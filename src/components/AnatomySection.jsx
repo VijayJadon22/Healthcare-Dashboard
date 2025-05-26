5c851e0ab302605c96ddd3c8b884fa854c72fc7b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-26 23:30:23 +0530

    Made changes in AnatomySection component

--- src/components/AnatomySection.jsx
+++ src/components/AnatomySection.jsx
@@ -4,15 +4,13 @@ import { FaHeart } from "react-icons/fa";
 
 const AnatomySection = () => {
   return (
-    <div className="relative w-full sm:w-3/5 flex items-center justify-center p-6 rounded-lg bg-[#f6faff]">
-      <img src={bodyImage} alt="Anantomy-image"  />
-      <div className="absolute top-30 -right-6 px-2 py-1 rounded-lg bg-blue-900  text-white flex items-center justify-between">
-        <span>❤️</span>
-        <span className="ml-1 text-xs">Healthy Heart</span>
+    <div className="relative w-full md:w-3/5 flex items-center justify-center p-6 rounded-lg bg-[#f6faff]">
+      <img className="" src={bodyImage} alt="Anantomy-image" width={120} />
+      <div className="absolute top-30 -right-6  p-1 rounded-lg bg-blue-900  text-white flex items-center justify-between">
+        <span className="ml-1 text-xs">❤️Healthy Heart</span>
       </div>
-      <p className="absolute bottom-25 left-0 px-2 py-1 rounded-lg bg-cyan-300 flex items-center justify-between text-[#3734a9]">
-        <span>🦵</span>
-        <span className="ml-1 text-xs">Healthy Leg</span>
+      <p className="absolute bottom-25 left-0 p-1 rounded-lg bg-cyan-300 flex items-center justify-between text-[#3734a9]">
+        <span className="ml-1 text-xs">🦵Healthy Leg</span>
       </p>
     </div>
   );
