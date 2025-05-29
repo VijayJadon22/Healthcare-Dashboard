3c42f4851de2d2419283f6eb87d334bc05f8ff34
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-29 15:28:46 +0530

    Implemnted Sidebar component and made it responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RxDashboard } from "react-icons/rx";
 import { VscHistory } from "react-icons/vsc";
 import { IoCalendarNumber } from "react-icons/io5";
-import { MdAddBox } from "react-icons/md";
+import { BsFillPlusSquareFill } from "react-icons/bs";
 import { FcStatistics } from "react-icons/fc";
 import { BsChatDotsFill } from "react-icons/bs";
 import { BsFillTelephoneFill } from "react-icons/bs";
@@ -10,25 +10,44 @@ import { IoSettings } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
-    <div className="hidden sm:block w-2/5 md:w-1/5 border text-gray-400 p-6 ">
-      <div className="w-1/2">
+    <div className="hidden sm:flex w-2/5 md:w-1/5  text-gray-400 p-6  flex-col justify-start ">
+      <div className=" flex flex-col justify-between h-1/3  pl-4">
         <h1 className="text-sm mb-2">General</h1>
-        <div className="flex items-center justify-start">
-          <RxDashboard color="blue" />
-          <span className="text-gray-600 font-semibold ml-2">Dashboard</span>
+        <div className="flex items-center justify-start text-blue-500">
+          <RxDashboard />
+          <span className=" font-semibold ml-2">Dashboard</span>
         </div>
         <div className="flex items-center justify-start">
           <VscHistory />
           <span className="ml-2">History</span>
         </div>
-        <p>Calender</p>
-        <p>Appointments</p>
-        <p>Statistics</p>
+        <div className="flex items-center justify-start">
+          <IoCalendarNumber />
+          <span className="ml-2">Calender</span>
+        </div>
+        <div className="flex items-center justify-start">
+          <BsFillPlusSquareFill />
+          <span className="ml-2">Appointments</span>
+        </div>
+        <div className="flex items-center justify-start">
+          <FcStatistics />
+          <span className="ml-2">Statistics</span>
+        </div>
       </div>
-      <div>
+      <div className="w-2/3 flex flex-col justify-between h-24  mt-6  pl-4">
         <h1 className="text-sm">Tools</h1>
-        <p>Chat</p>
-        <p>Support</p>
+        <div className="flex items-center justify-start">
+          <BsChatDotsFill />
+          <span className="ml-2">Chat</span>
+        </div>
+        <div className="flex items-center justify-start">
+          <BsFillTelephoneFill />
+          <span className="ml-2">Support</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-start mt-20  pl-4">
+        <IoSettings />
+        <span className="ml-2">Settings</span>
       </div>
     </div>
   );
