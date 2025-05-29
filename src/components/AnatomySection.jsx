7c13f54b604fa4497adb91816c45c5ddf8ad62dc
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-29 22:09:26 +0530

    Added comments to AnatomySection component

--- src/components/AnatomySection.jsx
+++ src/components/AnatomySection.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import bodyImage from "../assets/humanBody.png";
-import { FaHeart } from "react-icons/fa";
+import bodyImage from "../assets/humanBody.png"; // Importing the body image asset
+import { FaHeart } from "react-icons/fa"; // Importing heart icon (not used in the code)
 
 const AnatomySection = () => {
   return (
-    <div className="relative w-full md:w-3/5 flex items-center justify-center p-6 rounded-lg bg-[#f6faff]">
-      <img className="" src={bodyImage} alt="Anantomy-image" width={120} />
-      <div className="absolute top-30 -right-6  p-1 rounded-lg bg-blue-900  text-white flex items-center justify-between">
-        <span className="ml-1 text-xs">❤️Healthy Heart</span>
+    // Main container: positioned relatively, flexible width (full on small screens, 2/3 on medium screens)
+    <div className="relative w-full md:w-2/3 flex items-center justify-center p-6 rounded-lg bg-[#f6faff] shadow-md mt-2">
+      {/* Anatomical Body Image */}
+      <img className="" src={bodyImage} alt="Anatomy-image" />
+
+      {/* Healthy Heart Label */}
+      <div className="absolute top-30 -right-1 p-1 rounded-lg bg-blue-900 text-white flex items-center justify-between">
+        <span className="ml-1 text-xs p-1">❤️Healthy Heart</span>
       </div>
-      <p className="absolute bottom-25 left-0 p-1 rounded-lg bg-cyan-300 flex items-center justify-between text-[#3734a9]">
-        <span className="ml-1 text-xs">🦵Healthy Leg</span>
+
+      {/* Healthy Leg Label */}
+      <p className="absolute bottom-30 left-0 p-1 rounded-lg bg-cyan-300 flex items-center justify-between text-[#3734a9]">
+        <span className="ml-1 text-xs p-1">🦵Healthy Leg</span>
       </p>
     </div>
   );
